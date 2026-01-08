29985005aed27513348a82be38de552e5298197a
haripriya1190 <priyasuguraj@gmail.com>
2026-01-08 05:20:33 +0000

    Day 0: secure environment setup and Terraform provider validation

---
+++ terraform/environments/prod/variables.tf
@@ -0,0 +1,16 @@
+variable "pm_api_url" {
+  description = "Proxmox API endpoint"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox API token ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API token secret"
+  type        = string
+  sensitive   = true
+}
